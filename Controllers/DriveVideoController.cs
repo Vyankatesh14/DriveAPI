f9417c44c18a8dc246515d98c88edd3332f019b8
VYANKATESH DESHMUKH <126244294+Vyankatesh14@users.noreply.github.com>
2025-06-09 16:34:19 +0530

    2

---
+++ Controllers/DriveVideoController.cs
@@ -0,0 +1,36 @@
+﻿using DriveGoogleAPi.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading.Tasks;
+
+namespace DriveGoogleAPi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class DriveVideoController : ControllerBase
+    {
+        private readonly GoogleDriveService _driveService;
+
+        public DriveVideoController(GoogleDriveService driveService)
+        {
+            _driveService = driveService;
+        }
+
+        [HttpGet("download/{fileId}")]
+        public async Task<IActionResult> DownloadVideo(string fileId)
+        {
+            try
+            {
+                var stream = await _driveService.DownloadFileAsync(fileId);
+                var fileName = await _driveService.GetFileNameAsync(fileId);
+
+                // Using video/mp4 as default mime type, can be improved to detect dynamically
+                return File(stream, "video/mp4", fileName);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest($"Error downloading file: {ex.Message}");
+            }
+        }
+    }
+}
